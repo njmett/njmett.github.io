d7f274a7bc2884189fbf12322bc9d2e82f77e62a
Niklas Mettälä <njmett@utu.fi>
2024-07-21 23:12:12 +0300

    Get ready to upload

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'njmett', // Usually your GitHub org/user name.
   projectName: 'njmett.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
